957191cc7b09b611d9b22fb56e48b87f50db9f79
Chad Sakonchick <chadwicksakonchick@gmail.com>
2017-04-03 12:56:23 -0500

    Seeing if the /spaces route that shared with another route was conflicting

--- app.rb
+++ app.rb
@@ -44,8 +44,8 @@ get "/space/:record" do
   erb :space
 end
 
-get "/spaces/:venue_id" do
-  @spaces = Space.find_by venue__c(params[:venue_id])
+get "/venue/:venue_id" do
+  @spaces = Space.find_by_venue__c(params[:venue_id])
 
   erb :index
 end
