e35274f616991c1cabebd39893cc31b938864f14
Chad Sakonchick <chadwicksakonchick@gmail.com>
2017-04-28 13:40:20 -0500

    Trying the Post each Parent Space idea

--- app.rb
+++ app.rb
@@ -160,15 +160,20 @@ post "/hook/:booking/:venue/:calendar/:start/:end" do
     s.attributes.merge("booking": params[:booking],"calendar": params[:calendar],"start": params[:start],"end": params[:end])
   end
 
-  #@spaces.each do |s|
-  #  space = s.sfid
-  #  @included_spaces = Included_Space.where("belongs_to__c = ?", space)
-  #end
+  @spaces.each do |s|
+    space = s.sfid
+    @included_spaces = Included_Space.where("belongs_to__c = ?", space)
+    HTTParty.post("https://hooks.zapier.com/hooks/catch/962269/1efcdv/",
+    { 
+      :body => @included_spaces.to_json,
+      :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'}
+    })
+  end
 
-  @included_spaces = Space.joins(:venue)
-  .joins(:included_spaces)
-  .where(venue_id: params[:venue])
-  .select('spaces.name')
+  #@included_spaces = Space.joins(:venue)
+  #.joins(:included_spaces)
+  #.where(venue_id: params[:venue])
+  #.select('spaces.name')
 
   #Included_Space.joins(:space).where(:space{:venue => params[:venue]})
 
