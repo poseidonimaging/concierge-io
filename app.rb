635571ced42c9c9cb532ee4fb40a8c4f23cb5d6e
Chad Sakonchick <chadwicksakonchick@gmail.com>
2017-05-26 17:25:58 -0500

    Fix response headers

--- app.rb
+++ app.rb
@@ -242,7 +242,7 @@ post "/hook/retrieve/spaces" do
     puts "Sent Spaces Hook"
     puts "Writing Spaces"
     @spaces.to_json
-    [200, {"status"=>"success"}, "Success"] #yeah it should work
+    [200, {}, "status"=>"success"] #this is how it should be
   else
     [400, {}, "Authorization Failed"]
   end
